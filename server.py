1a5ef3ca09fca0d769a0c90086159a63ff8b480b
Ron Turetzky <turetzkyron@gmail.com>
2024-10-16 11:12:14 +0300

    chore: adding signing of reqs by server

--- server.py
+++ server.py
@@ -3,10 +3,12 @@ import requests
 import urllib3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
-import ssl
 import os
 from dotenv import load_dotenv
 import json 
+import time
+from web3.auto import w3
+from eth_account.messages import encode_defunct
 def log(message):
     file = open("server.log.txt", "a")
     file.write(message + "\n")
@@ -29,15 +31,20 @@ def get_operator() -> str | None:
 
 # Proxy request handler
 class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def __init__(self, *args, **kwargs):
         load_dotenv()
+        if os.getenv('PRIVATE_KEY') is None or os.getenv('MAX_OPERATOR_RETRY_ATTEMPTS') is None:
+            raise ValueError("PRIVATE_KEY and MAX_OPERATOR_RETRY_ATTEMPTS must be set")
+        self.private_key = os.getenv('PRIVATE_KEY')
+        self.max_attempts = int(os.getenv('MAX_OPERATOR_RETRY_ATTEMPTS', 5))
+        super().__init__(*args, **kwargs)
+    def do_GET(self):
         # initalize the max attempts using the environment variable MAX_OPERATOR_RETRY_ATTEMPTS or default to 5 
-        max_attempts = int(os.getenv('MAX_OPERATOR_RETRY_ATTEMPTS', 5))
         attempts = 0
         operator = None
         target_url = None
 
-        while attempts < max_attempts:
+        while attempts < self.max_attempts:
             operator = get_operator()
             if operator is None:
                 self.send_error(500, "No operators available.")
@@ -52,15 +59,21 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
                 log(f"Operator is not live: {target_url}. Trying another operator.")
                 attempts += 1
         
-        if attempts == max_attempts:
+        if attempts == self.max_attempts:
             self.send_error(500, "No live operators available.")
             return
 
-        # Return the target URL in the response
+        # Return the target URL and signature in JSON format
         self.send_response(200)  # OK status
-        self.send_header('Content-type', 'text/plain')  # Set content type to plain text
+        self.send_header('Content-type', 'application/json')  # Set content type to JSON
         self.end_headers()
-        self.wfile.write(target_url.encode('utf-8'))  # Write the target URL in the response body
+        signature , timestamp = self.generate_signature(target_url)
+        response_data = {
+            "node_url": target_url,
+            "timestamp": timestamp,
+            "signature": signature
+        }
+        self.wfile.write(json.dumps(response_data).encode('utf-8'))  # Write JSON response
 
     def liveness_check(self, url):
         """Check if the target URL is live by sending a HEAD request."""
@@ -69,6 +82,13 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
             return response.status_code == 200
         except requests.RequestException:
             return False
+    def generate_signature(self, target_url) -> str:
+        """Generate a signature for the given target URL."""
+        timestamp = int(time.time())
+        message = f"{target_url},{timestamp}"
+        message = encode_defunct(text=message)
+        signature =  w3.eth.account.sign_message(message, private_key=self.private_key)
+        return signature.signature.hex(), timestamp
 
 def run(server_class=HTTPServer, handler_class=ProxyHTTPRequestHandler, port=8080):
     # Ignore SSL certificate verification
